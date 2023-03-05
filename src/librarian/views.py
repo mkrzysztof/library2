39331ab6c49031ac0bfddc119e4ac777d27e3312
krzysio <>
2023-03-05 02:35:10 +0100

    utworzenie szkieletu

--- src/librarian/views.py
+++ src/librarian/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class Librarian(View):
+    pass
+
+
+class Search(View):
+    pass
+
+
+class BookReturn(View):
+    pass
+
+
+class UserProfile(View):
+    pass
+
+
+class LendBook(View):
+    pass
+
+
+class LoanExtension(View):
+    pass
